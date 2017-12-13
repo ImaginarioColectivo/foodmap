97c5090310bbd1a15819fdf49467c04788a6557c
ImaginarioColectivo <marjourzua@gmail.com>
2017-12-12 21:22:09 -0300

    primera seccion, vista splash, filtro comida, galeria de fotos

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+$().ready(function() {
+        $("#splash").fadeIn(500).fadeOut(2000);
+      });
+
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+});
+
+/* query galeria comida*/
+
+$(document).ready(function(){
+	$(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+        
+    if(value == "all")
+        {
+//$('.filter').removeClass('hidden');
+      $('.filter').show('1000');
+    }
+    else
+    {
+//$('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//$(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter").not('.'+value).hide('3000');
+      $('.filter').filter('.'+value).show('3000');
+            
+    }
+	});
+    
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
